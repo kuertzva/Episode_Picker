44114756136dd282b57c34aba9a55dfb8dfe61b7
kuertzva <kuertzva@miamioh.edu>
2020-01-08 12:43:34 -0500

    added build folder

---
+++ build/precache-manifest.5d84b6ea11a5158c00fbffb70953c203.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bf136c49fd2fef207e1d3ea96ce024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c578b1b2942fc735bf",
+    "url": "/static/css/main.55680a11.chunk.css"
+  },
+  {
+    "revision": "3f2dbdad40f9869084bf",
+    "url": "/static/js/2.82e0c83d.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/static/js/2.82e0c83d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0c578b1b2942fc735bf",
+    "url": "/static/js/main.2dba1f3a.chunk.js"
+  },
+  {
+    "revision": "964b37a9dde9f5219e2d",
+    "url": "/static/js/runtime-main.c117c8f4.js"
+  },
+  {
+    "revision": "eb785569e2d735124bdcf040a239447f",
+    "url": "/static/media/commando.eb785569.ttf"
+  },
+  {
+    "revision": "4b47fcc0ec67f38371d04279b7ad39ab",
+    "url": "/static/media/telegrama_raw-webfont.4b47fcc0.woff2"
+  }
+]);
